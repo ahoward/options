fc31c8ce71467bb7b34affdd8dc9d92ac93a01aa
Peter Williams <pezra@youthful-indiscretion.local>
2009-08-20 01:01:00 +0800

    added validation sample; fixed typo in a.rb comments
    Signed-off-by: ahoward <ara.t.howard@gmail.com>

--- samples/a.rb
+++ samples/a.rb
@@ -1,6 +1,6 @@
 require 'options'
 
-# options.rb makes is super easy to deal with keyword options in a safe and
+# options.rb makes it super easy to deal with keyword options in a safe and
 # easy way.
 #
 
